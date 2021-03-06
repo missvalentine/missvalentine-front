6a4ff804319a71017511080893c9e9d81313d15f
akshat360 <akshataggarwal360@gmail.com>
2021-03-06 21:43:45 +0530

    home fix

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "295012d0fc17b4ca50bd";
+/******/ 	var hotCurrentHash = "527a06c022894792511c";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
--- components/OurDirectors.js
+++ components/OurDirectors.js
@@ -6,7 +6,7 @@ export default function OurDirectors() {
       name: 'Vikas Jain',
       desc: 'Have 10year + experinece in same field',
       img:
-        'https://lh3.googleusercontent.com/proxy/YMp4w6Y3nCxW6BzhO8CqEOqi-mY_Rhy9cGz6q5YdjmAxkvBL9C-ah1m8ICcV1y0pwgKzCElGiiEqKLxF13GOaQ-c7EgVWAHQZcYm1Bi6sn19L2OhuO6gaA',
+        'https://user-images.githubusercontent.com/5709133/50445980-88299a80-0912-11e9-962a-6fd92fd18027.png',
     },
     {
       name: 'Sidhart Jain',
@@ -18,7 +18,7 @@ export default function OurDirectors() {
       name: 'Vikas Jain',
       desc: 'Have 10year + experinece in same field',
       img:
-        'https://lh3.googleusercontent.com/proxy/YMp4w6Y3nCxW6BzhO8CqEOqi-mY_Rhy9cGz6q5YdjmAxkvBL9C-ah1m8ICcV1y0pwgKzCElGiiEqKLxF13GOaQ-c7EgVWAHQZcYm1Bi6sn19L2OhuO6gaA',
+        'https://user-images.githubusercontent.com/5709133/50445980-88299a80-0912-11e9-962a-6fd92fd18027.png',
     },
   ];
 
